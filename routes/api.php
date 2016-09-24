b3dea5a8f04a8d1091612211e86d44078241e579
Sezer Tunca <sezertunca@gmail.com>
2016-09-24 13:41:03 +0100

    Add more tests, update routes to not use redundant recipe word

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 Route::group(['prefix' => 'v1', 'namespace' => 'v1'], function() 
 {
 	Route::get('/recipes', 'RecipeController@index');
-	Route::get('recipes/recipe/{id}', 'RecipeController@show');
+	Route::get('recipes/{recipeId}', 'RecipeController@show');
 	Route::post('recipes/store', 'RecipeController@store');
-	Route::post('recipes/recipe/{id}/update', 'RecipeController@update');
-	Route::post('recipes/recipe/{id}/rate', 'RecipeController@rate');
+	Route::post('recipes/{recipeId}/update', 'RecipeController@update');
+	Route::post('recipes/{recipeId}/rate', 'RecipeController@rate');
 	Route::get('recipesForCuisine/{cuisine}', 'RecipeController@getRecipesForCuisine');
 });
--- tests/RecipeTest.php
+++ tests/RecipeTest.php
@@ -1,19 +1,33 @@
 <?php
 
+// User Arrange, Act, Assert
 
 class RecipeTest extends ApiTester
 {
     /**
      * @test
-     * 
      */
     public function it_fetches_all_recipes()
     {
-        // Arrange
-        // Act
         $this->getJSON('api/v1/recipes');
-        // Assert
-
         $this->assertResponseOk();
+    }
+
+    /**
+     * @test
+     */
+    public function it_fetches_single_recipe_that_exists()
+    {
+        $this->getJSON('api/v1/recipes/1');
+        $this->assertResponseOk();
+    }
+
+    /**
+     * @test
+     */
+    public function it_fetches_single_recipe_that_not_exist()
+    {
+        $this->getJSON('api/v1/recipes/100');
+        $this->assertResponseStatus(404);
     }
 }
