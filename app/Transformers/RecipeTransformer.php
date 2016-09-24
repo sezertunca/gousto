8dd356d0443eabdfbd91f7acd052ba761235a7b6
Sezer Tunca <sezertunca@gmail.com>
2016-09-24 11:48:58 +0100

    Add all the fields to transform return

--- app/Transformers/RecipeTransformer.php
+++ app/Transformers/RecipeTransformer.php
@@ -9,7 +9,32 @@ class RecipeTransformer extends Transformer
 		return 
 		[
 			'id' => $recipe->id,
-			'title' => $recipe->title
+			'created_at' => $recipe->created_at,
+			'updated_at' => $recipe->updated_at,
+			'box_type' => $recipe->box_type,
+			'title' => $recipe->title,
+			'slug' => $recipe->slug,
+			'short_title' => $recipe->short_title,
+			'marketing_description' => $recipe->marketing_description,
+			'calories_kcal' => $recipe->calories_kcal,
+			'protein_grams' => $recipe->protein_grams,
+			'fat_grams' => $recipe->fat_grams,
+			'carbs_grams' => $recipe->carbs_grams,
+			'bulletpoint1' => $recipe->bulletpoint1,
+			'bulletpoint2' => $recipe->bulletpoint2,
+			'bulletpoint3' => $recipe->bulletpoint3,
+			'recipe_diet_type_id' => $recipe->recipe_diet_type_id,
+			'season' => $recipe->season,
+			'base' => $recipe->base,
+			'protein_source' => $recipe->protein_source,
+			'preparation_time_minutes' => $recipe->preparation_time_minutes,
+			'shelf_life_days' => $recipe->shelf_life_days,
+			'equipment_needed' => $recipe->equipment_needed,
+			'origin_country' => $recipe->origin_country,
+			'recipe_cuisine' => $recipe->recipe_cuisine,
+			'in_your_box' => $recipe->in_your_box,
+			'gousto_reference' => $recipe->gousto_reference,
+			'rate' => $recipe->rate
 		];
     }
 }
