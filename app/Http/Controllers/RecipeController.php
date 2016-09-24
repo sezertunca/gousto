fe0ec5c6d027295f7e8c9d91f89b395d0b21a96a
Sezer Tunca <sezertunca@gmail.com>
2016-09-24 10:17:37 +0100

    Add the Restful functions

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -4,9 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
-
 class RecipeController extends Controller
 {
-    //
+    // Show all recipes
+    public function index()
+    {
+    }
+
+    // Fetch a recipe by id
+	public function show($recipeId)
+	{
+	}
+
+	// Rate an existing recipe between 1 and 5
+	public function rate(Request $request, $recipeId)
+	{
+	}
+
+	// Update an existing recipe
+	public function update(Request $request, $recipeId)
+	{
+	}
+
+	// Store a new recipe
+	public function store(Request $request)
+	{
+	}
+
+	// Fetch all recipes for a specific cuisine (should paginate)
+	public function getRecipesForCuisine($cuisine)
+	{
+	}
+
+ 
 }
